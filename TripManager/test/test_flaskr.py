0791df36dd6ea231004bda0e1de696bbff266700
Josh <jpal.lee7e@gmail.com>
2018-05-01 17:10:36 -0700

    Adding first unit test

--- TripManager/test/test_flaskr.py
+++ TripManager/test/test_flaskr.py
@@ -1,16 +1,42 @@
 import os
 import sys
-import unittest
+import pytest
 import tempfile
+import TripManager
 
 sys.path.append('/home/josh/development/TripManager')
 """
 Need to import the main file that contains the instance of the flask app that I want to test. 
 At least until I restructure this whole project to be more standardized.
 """
-from main import app
-from models import db
 
+
+from TripManager.main import app
+from TripManager.models import db
+
+@pytest.fixture
+def client():
+	db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+	app.config['TESTING'] = True
+	client = app.test_client()
+
+	
+	db.init_app(app)
+
+	yield client
+
+	os.close(db_fd)
+	os.unlink(app.config['DATABASE'])
+
+
+def test_empty_db(client):
+	"""Start with a blank database."""
+
+	rv = client.get('/')
+	assert b'This is a test page' in rv.data
+
+
+"""
 class FlaskrTestCase(unittest.TestCase):
 
 	def setUp(self):
@@ -34,3 +60,4 @@ class FlaskrTestCase(unittest.TestCase):
 
 if __name__=='__main__':
 	unittest.main()
+"""
