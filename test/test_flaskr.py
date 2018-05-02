944cb160f1a763347a670d0bc67824d62ad15fd4
Josh <jpal.lee7e@gmail.com>
2018-05-01 17:32:12 -0700

    Adding pip install -e . to travis config

--- test/test_flaskr.py
+++ test/test_flaskr.py
@@ -10,9 +10,9 @@ Need to import the main file that contains the instance of the flask app that I 
 At least until I restructure this whole project to be more standardized.
 """
 
-
-from .TripManager.main import app
-from .TripManager.models import db
+import TripManager
+from TripManager.main import app
+from TripManager.models import db
 
 @pytest.fixture
 def client():
