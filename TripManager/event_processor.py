5b31cd409c85beddf27a10261876aa131786fb40
Josh <jpal.lee7e@gmail.com>
2018-06-24 16:47:43 -0700

    Modified set sass event handling

--- TripManager/event_processor.py
+++ TripManager/event_processor.py
@@ -1,21 +1,24 @@
 from flask import (make_response)
+from TripManager.models import db, Entry, User
 
 
 class EventProcessor:
     
     def __init__(self, event_type, slack_event):
-        self.names = ['andrew', 'alex', 'nick', 'brendan', 'millian']
+        users = User.query.filter().all()
+        self.names = []
+        for user in users:
+            self.names.append(user.real_name)
         self.response = "Message received, but nothing was done"
         self.user_id = slack_event["event"]["user"]
         self.channel_id = slack_event["event"].get("channel")
 
         if event_type == "message":
-            self.message_content = slack_event["event"]["text"].lower()
+            self.message_content = slack_event["event"]["text"]
         elif event_type == "app_mention":
             self.message_content = slack_event["event"]['text']
 
     def handle_message(self, pyBot):
-
         if 'add' in self.message_content:
             pyBot.add_camping_item(self.channel_id, self.user_id, self.message_content)
             self.response = "Items were added"
@@ -36,27 +39,10 @@ class EventProcessor:
             self.response = "Listed camping items purchased"
             return make_response(self.response, 200)
 
-        elif 'set sass for' in self.message_content:
-            pyBot.set_sass(self.channel_id, self.user_id, self.message_content)
-            self.response = "Sass has been set"
+        elif 'set sass' in self.message_content:
+            pyBot.prepare_sass(self.channel_id, self.user_id, self.message_content)
+            self.response = "Sass is being set"
             return make_response(self.response, 200)
-
-        elif 'sass' in self.message_content:
-            pyBot.sass(self.channel_id, self.user_id, self.message_content)
-            self.response = "Sass has been delivered"
-            return make_response(self.response, 200)
-
-        elif 'test send attachment' in self.message_content:
-            print('GOT IT - ---------------------')
-            pyBot.order_test(self.channel_id, self.user_id)
-            self.response = "Sent test attachment"
-            return make_response(self.response, 200)
-
-        elif 'test send input' in self.message_content:
-            pyBot.custom_enter_test(self.channel_id, self.user_id)
-            self.response = "Sent test attachment"
-            return make_response(self.response, 200)
-            
             
         elif any(name in self.message_content for name in self.names):
             """
@@ -65,9 +51,10 @@ class EventProcessor:
             """
             for name in self.names:
                 if name in self.message_content:
-                    pyBot.sass(self.channel_id, self.user_id, name)
+                    user = User.query.filter_by(real_name=name).first()
+                    pyBot.sass(self.channel_id, user.slack_id)
                     self.response = "Sass was dispensed"
-                    return make_response(response, 200)
+                    return make_response(self.response, 200)
         else:
             return make_response(self.response,
             200,)
