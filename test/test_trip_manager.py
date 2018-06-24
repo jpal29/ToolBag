1085ab157497a2c5680ee597950cdb94ce8db71d
Josh <jpal.lee7e@gmail.com>
2018-06-24 16:45:40 -0700

    Added a test case setting sass

--- test/test_trip_manager.py
+++ test/test_trip_manager.py
@@ -18,6 +18,7 @@ from TripManager.models import db
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+
 @pytest.fixture
 def client():
 	db_fd, app.config['DATABASE'] = tempfile.mkstemp()
@@ -33,13 +34,6 @@ def client():
 
 	os.close(db_fd)
 	os.unlink(app.config['DATABASE'])
-
-
-def test_home_page(client):
-	"""Start with a blank database."""
-
-	rv = client.get('/')
-	assert b'This is a test page' in rv.data
 
 
 #Test to make sure that the challenge event on /listening route works
@@ -59,7 +53,10 @@ def test_challenge(client):
     	"type": "url_verification"
 	}
 
-	response = client.post('/listening', data=json.dumps(data), headers=headers)
+	response = client.post('/listening', 
+								data=json.dumps(data), 
+								headers=headers)
+
 	assert response.content_type == mimetype
 	assert response.status_code == 200
 
@@ -70,28 +67,53 @@ def test_challenge(client):
 def test_add_item(client):
 	add_request = request_wrapper("add foo")
 
-	response = client.post('/listening', data=json.dumps(add_request.data), headers=add_request.headers)
+	response = client.post('/listening', 
+								data=json.dumps(add_request.data), 
+								headers=add_request.headers)
+
 	assert "Items were added" in response.data
 	assert response.status_code == 200
 
 def test_remove_item(client):
 	remove_request = request_wrapper("remove foo")
-	response = client.post('/listening', data=json.dumps(remove_request.data), headers=remove_request.headers)
+	response = client.post('/listening', 
+								data=json.dumps(remove_request.data), 
+								headers=remove_request.headers)
+
 	assert "Items were removed from list of needed items and added to purchased item list"
 
 def test_list_needed_items(client):
 	list_request = request_wrapper("list needed items")
 
-	response = client.post('/listening', data=json.dumps(list_request.data), headers=list_request.headers)
+	response = client.post('/listening', 
+								data=json.dumps(list_request.data), 
+								headers=list_request.headers)
+
 	assert "Listed camping items needed" in response.data
 	assert response.status_code == 200
 
 def test_list_purchased_items(client):
 	list_request = request_wrapper("list purchased items")
 
-	response = client.post('/listening', data=json.dumps(list_request.data), headers=list_request.headers)
+	response = client.post('/listening', 
+								data=json.dumps(list_request.data), 
+								headers=list_request.headers)
+
 	assert "Listed camping items purchased" in response.data
 	assert response.status_code == 200
+
+def test_set_sass(client):
+	set_sass_request = request_wrapper("set sass")
+
+	response = client.post('/listening', 
+								data=json.dumps(set_sass_request.data), 
+								headers=set_sass_request.headers)
+	
+	
+	assert "Sass is being set" in response.data
+	assert response.status_code == 200
+
+
 
 
 
